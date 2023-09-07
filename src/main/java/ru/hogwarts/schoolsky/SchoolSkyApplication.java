c7ff16150aeb61a7cbd071052461bc56e388ef36
Lexey9105 <umpp120@gmail.com>
2023-09-07 23:16:17 +0300

    Initial Commit

---
+++ src/main/java/ru/hogwarts/schoolsky/SchoolSkyApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSkyApplication.class, args);
+	}
+
+}
