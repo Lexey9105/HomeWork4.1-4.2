1d713d6838ee1e15276df37029a72ba9e33b8a4c
Lexey9105 <umpp120@gmail.com>
2023-09-28 10:27:22 +0300

    ДЗ 4.5 правки внесены

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -3,6 +3,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 @RestController
@@ -18,10 +19,9 @@ public class InfoController {
         return "Port is: " + environment.getProperty("server.port");
     }
     @GetMapping("intByStream")
-    public Integer intByStream(int limit){
-        return Stream.iterate(1, a -> a + 1)
-                .limit(1_000_000)
-                .parallel()
-                .reduce(0, Integer::sum);
+    public long intByStream(int limit){
+        long result= LongStream.range(1,limit)
+                .sum();
+        return result;
     }
 }
